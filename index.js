74f58589eba7f94448c7c6abf65e1a374167f5da
Daniil <traitnil@gmail.com>
2022-12-07 23:51:39 +0200

    Declaration variables

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-console.log(1);
-console.log('hello');
-console.log(true);
-console.log(undefined);
-console.log(null);
+let box;
+box = 3;
+
+console.log(box);
+
+let value = 5;
+
+console.log(value);
+
+
+
+// console.log(1);
+// console.log('hello');
+// console.log(true);
+// console.log(undefined);
+// console.log(null);
