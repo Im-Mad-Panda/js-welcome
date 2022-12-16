690203c8d1ffa63162111d81dc2e39b95c7e6943
Daniil <traitnil@gmail.com>
2022-12-17 00:51:27 +0200

    Assignment operators

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-// let a = 'Daniil';
-// let b = 'Obdymko';
 
-
-// console.log(a > b);
-
-let age = 15; 
-
-console.log(age > 10 && age < 20);
-
-console.log(age < 10 || age > 20);
-
-console.log(age > 10 || age > 20);
      
