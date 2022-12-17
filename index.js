ae3fedd0738321aaadcf7e9f8f61317b36b6977a
Daniil <traitnil@gmail.com>
2022-12-18 01:26:31 +0200

    practiceNum

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+// alert - модалка с сообщением;
+// prompt - модалка с импутом, куда можно ввести сообщение;
+// confirm - модалка с подтверждением или отменойж
+
 let a = 5;
-let b = 10;
-let c = 5;
-let d = '5';
+let b = 4;
 
-console.log(a == b);
-console.log(a === b);
+console.log(a * b);
 
-console.log(c == d);
-console.log(c === d);
-     
+let c = 10;
+let d = 2;
+
+console.log(c / d);
+
+let e = 55;
+let f = 27;
+
+console.log(e + f);
+
+let number = 33;
+let bool = false;
+let str = javascript;
+let strNum = "100";
+
+console.log(number, bool, str, strNum);
+
+let num = 1;
+num++;
+num--;
+num += 1;
+num -=1;
+num *= 11;
+num /= 11;
+
