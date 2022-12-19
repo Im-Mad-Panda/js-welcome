f7fcfecddf2c1e6084c5fd4560588c4a6920137f
Daniil <traitnil@gmail.com>
2022-12-20 00:35:49 +0200

    Function tasks resolve

--- index.js
+++ index.js
@@ -45,22 +45,50 @@ Arrow Function:
 const имя_функции = (параметры) => {тело_функции}
 */
 
+/* const userName = prompt('Your name, please?');
+
+const sayHelloTo = function(name){
+const alertText = 'Hello, ' + name;
+alert(alertText);
+}
+
+sayHelloTo(userName);
+*/
+
+
+// Task 1
 
 const inputValue1 = Number(prompt('Type first number'));
 const inputValue2 = Number(prompt('Type second number'));
 
-const getSumOfTwo = (Value1, Value2) =>{
-    let result;
-    if (isNaN(Value1) && isNaN(Value2)){
-        result = 'Wrong';
-    } else {
-        result = Value1 + Value2;
-    }
-
-debugger;
-
-    return result;
+const maxValue = function(value1, value2){
+  if(value1 > value2){
+return value1;
+  } else{
+return value2;
+  }
 }
 
-const results = getSumOfTwo(inputValue1, inputValue2);
-alert(results);
+const result = maxValue(inputValue1, inputValue2);
+alert(result);
+
+// Task 2
+
+const minValue = function(value1, value2){
+  if(value1 < value2){
+return
+  } else{
+return value2;
+  } 
+}
+
+const smaller = minValue(5, 3);
+alert(smaller);
+
+// Task 3
+
+function isEven (value1){
+    return value1 % 2 === 0
+}
+
+console.log(isEven(4));
