f488b9e5b5e55f49013e04d041fd8924bcdfa209
Daniil <traitnil@gmail.com>
2023-01-03 00:50:28 +0200

    Switch-case

--- index.js
+++ index.js
@@ -6,19 +6,56 @@ if (услорвие){
 
 */
 
+/*const option = +prompt(
+  "Выборите позицию меню \n 1 - Чай \n 2 - Кофе \n 3 - Сок \n 4 - Лимонад \n 5 - Вода"
+);
 
-const number = +prompt('Type your number');
-
-if (isNaN(number)){
-    console.log('Некоректное число');
-} else if(number % 5 === 0){
-    console.log('Делится на 5')
-}else if(number % 3 === 0){
-    console.log('Делится на 3')
-}else if(number % 2 === 0){
-    console.log('Делится на 2')
+if (option === 1) {
+  console.log("Вот ваш Чай");
+} else if (option === 2) {
+  console.log("Вот ваш Кофе");
+} else if (option === 3) {
+  console.log("Вот ваш Сок");
+} else if (option === 4) {
+  console.log("Вот ваш Лимонад");
+} else if (option === 5) {
+  console.log("Вот ваш Вода");
 } else{
-    console.log('Не делиться не на 5 не на 3 не на 2')
-}
+    console.log('Такого нет в меню! Сорян)')
+}*/
 
-console.log('Конец скрипта')
+/*switch (значение){
+  case вариант:
+      что делать, если вариант подходит;
+      break
+  case вариант2:
+      что делать, если вариант подходит;
+      break
+  ...
+  default:
+    конец
+}*/
+
+const userInput = prompt(
+  "Выборите позицию меню \n 1 - Чай \n 2 - Кофе \n 3 - Сок \n 4 - Лимонад \n 5 - Вода"
+);
+
+switch (userInput) {
+  case "1":
+    console.log("Вот ваш Чай");
+    break
+  case "2":
+    console.log("Вот ваш Кофе");
+    break
+  case "3":
+    console.log("Вот ваш Сок");
+    break
+  case "4":
+    console.log("Вот ваш Лимонад");
+    break
+  case "5":
+    console.log("Вот ваш Вода");
+    break
+  default:
+    console.log("Такого нет в меню! Сорян)");
+}
