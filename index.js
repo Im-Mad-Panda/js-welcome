d9b9010fd1ec5529aba6fbccbe975e46cf98c8c4
Daniil <traitnil@gmail.com>
2023-01-05 01:21:42 +0200

    Greeting function resolve

--- index.js
+++ index.js
@@ -1,116 +1,49 @@
-/*Else - if
-
-if (услорвие){
-    else if (новое условие)
-}
-
-*/
-
-/*const option = +prompt(
-  "Выборите позицию меню \n 1 - Чай \n 2 - Кофе \n 3 - Сок \n 4 - Лимонад \n 5 - Вода"
-);
-
-if (option === 1) {
-  console.log("Вот ваш Чай");
-} else if (option === 2) {
-  console.log("Вот ваш Кофе");
-} else if (option === 3) {
-  console.log("Вот ваш Сок");
-} else if (option === 4) {
-  console.log("Вот ваш Лимонад");
-} else if (option === 5) {
-  console.log("Вот ваш Вода");
-} else{
-    console.log('Такого нет в меню! Сорян)')
-}*/
-
-/*switch (значение){
-  case вариант:
-      что делать, если вариант подходит;
-      break
-  case вариант2:
-      что делать, если вариант подходит;
-      break
-  ...
-  default:
-    конец
-}*/
-
-/*const userInput = prompt(
-  "Выборите позицию меню \n 1 - Чай \n 2 - Кофе \n 3 - Сок \n 4 - Лимонад \n 5 - Вода"
-);
-
-switch (userInput) {
-  case "1":
-    console.log("Вот ваш Чай");
-    break
-  case "2":
-    console.log("Вот ваш Кофе");
-    break
-  case "3":
-    console.log("Вот ваш Сок");
-    break
-  case "4":
-    console.log("Вот ваш Лимонад");
-    break
-  case "5":
-    console.log("Вот ваш Вода");
-    break
-  default:
-    console.log("Такого нет в меню! Сорян)");
-}*/
-
-const number1 = +prompt("Первое число");
+/*const number1 = +prompt("Первое число");
 const number2 = +prompt("Второе число");
 const operator = prompt("Выберети оператор: \n + \n - \n * \n /");
-
 
 const res = calculate(number1, number2, operator);
 console.log(res);
 
-
-function sum (a,b){
+function sum(a, b) {
   return a + b;
 }
-function sub (a,b){
+function sub(a, b) {
   return a - b;
 }
-function mult (a,b){
+function mult(a, b) {
   return a * b;
 }
-function div (a,b){
+function div(a, b) {
   return a / b;
 }
 
+function calculate(num1, num2, userMode) {
+  let f;
 
-function calculate(num1, num2, userMode){
-let f;
+  switch (userMode) {
+    case "+":
+      f = sum;
+      break;
+    case "-":
+      f = sub;
+      break;
+    case "*":
+      f = mult;
+      break;
+    case "/":
+      f = div;
+      break;
+  }
 
-switch (userMode) {
-  case "+":
-    f = sum;
-    break;
-  case "-":
-    f = sub;
-    break;
-  case "*":
-    f = mult;
-    break;
-  case "/":
-    f = div;
-    break;
-}
-
-if(typeof f === 'function'){
-  const result = f(num1, num2)
-  return result;
-} else {
-  console.log('Oppps!');
-  return null;
-}
-}
-
-
+  if (typeof f === "function") {
+    const result = f(num1, num2);
+    return result;
+  } else {
+    console.log("Oppps!");
+    return null;
+  }
+}*/
 
 /*const getDecade = function (day) {
   switch (day) {
@@ -152,3 +85,25 @@ if(typeof f === 'function'){
       return null;
   }
 };*/
+
+/* Function Declaration:
+function имя_функции(параметры_функции){
+    тело_функции
+}
+Function Expression:
+const имя_функции = function (параметры){
+    тело функции
+}
+Arrow Function:
+const имя_функции = (параметры) => {тело_функции}
+*/
+
+// function greeting(name)
+const greeting = function(name){
+const checkedName = name ? name : 'Аноним';
+console.log('Привет, ' + checkedName);
+}
+
+const userName =prompt('Ведите ваше имя');
+
+greeting(userName);
