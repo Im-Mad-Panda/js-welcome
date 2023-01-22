12f353c09c06a75344eb9e47000fc9a60d81efbf
Daniil <traitnil@gmail.com>
2023-01-23 01:16:50 +0200

    Object CRUD

--- index.js
+++ index.js
@@ -1,57 +1,36 @@
 "use strict";
 
-/*function getSumOdd(from, to) {
-  let sum = 0;
+/*const cat = {}; 
 
-  for (let i = from; i <= to; i++) {
-    if (i % 2 === 0) {
-      sum = sum + i;
-    }
-  }
-  return sum;
-}
-const result = getSumOdd(3, 7);
-console.log(result);*/
+const cat2 = Object();
 
+const cat3 = new Object();*/
 
-/*function factorial(number) {
+const cat = {
+  name: 'Barsik',
+  age: 4,
+  color: 'ginger',
+  weigh:  5,
+  breed: 'Cheshirskiy',
+  isMale: true,
+  isSleeping: false
+};
 
-const MAX_TRY_FACTORIAL = 250;
-
-if (isNaN(number)) {
-  return false;
-}
-if (number === 1 || number === 0) {
-  return 1;
-}
-if (number > MAX_TRY_FACTORIAL){
-  return false;
-}
-
-let result = 1n;
-
-  for (let i = 1; i <= number; i++) {
-    result *= BigInt(i);
-  }
-  return result;
-}
-const res = factorial(4);
-console.log(res);*/
+// alert(cat.name);
 
 
-function getPower(base, power){
+cat.age++; // cat.age = cat.age + 1;
 
-  if (power === 0) {
-    return 1;
-  }
-  if (number === 1) {
-    return base;
-  }
 
-let result = 1;
+/*
 
-for(let i = 1; i <= power; i++){
-  result = result * base;
-}
-return result;
-}
+C - create
+R - read
+E - update
+D - delete 
+
+*/
+
+delete cat.isSleeping; 
+
+
