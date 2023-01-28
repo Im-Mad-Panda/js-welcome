efaf917b76495fd09d261998dfdd76711cdc0770
Daniil <traitnil@gmail.com>
2023-01-29 01:32:49 +0200

    Url temlate string task resolve

--- index.js
+++ index.js
@@ -11,20 +11,47 @@
 
 //  console.log(`Sum is ${2 + 2}`);
 
-number1 = 4;
-number2 = 5;
+// number1 = 4;
+// number2 = 5;
 
 
-function getDecription(){
-    return 'of these numders';
+// function getDecription(){
+//     return 'of these numders';
+// }
+
+
+// alert(`Sum ${getDecription()}
+// ${number1} + ${number2} = ${number1 + number2}`);
+// alert(`Sub ${getDecription()}
+// ${number1} - ${number2} = ${number1 - number2}`);
+// alert(`Multiplication ${getDecription()}
+// ${number1} * ${number2} = ${number1 * number2}`);
+// alert(`Division ${getDecription()}
+// ${number1} / ${number2} = ${number1 / number2}`);
+
+// const user = {
+//     name: 'Test',
+//     lastName: 'Testorovich',
+//     age: 18,
+//     isMale: true,
+// }
+
+// function sayHello(userObject){
+// console.log(`Hello, ${userObject.name} ${userObject.lastName}`)
+// }
+
+// sayHello(user);
+
+// https://github.com/Im-Mad-Panda
+
+
+const PROTOCOL = 'http://';
+const SERVER_IP = 'github.com';
+// const PORT = '3000';
+
+function createQuery(absPath){
+    const url =`${PROTOCOL}${SERVER_IP}${absPath}`
+    return url;
 }
 
-
-alert(`Sum ${getDecription()}
-${number1} + ${number2} = ${number1 + number2}`);
-alert(`Sub ${getDecription()}
-${number1} - ${number2} = ${number1 - number2}`);
-alert(`Multiplication ${getDecription()}
-${number1} * ${number2} = ${number1 * number2}`);
-alert(`Division ${getDecription()}
-${number1} / ${number2} = ${number1 / number2}`);
+console.log(createQuery('/Im-Mad-Panda'));
