140a71999e2e27c6901a1a5087d535b285e8a7c9
Daniil <traitnil@gmail.com>
2023-02-03 00:39:46 +0200

    This issue

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-function Cat(name, age, breed, color, isMale){
-    this.name = name;
-    this.age = age;
-    this.breed = breed;
-    this.color = color;
-    this.isMale = isMale;
+'use strict'
+
+const obj = {};
+
+function MyObj(){
+
+    this.prop = 'value';
 }
 
-const cat1 = new Cat('Barsik', 5, 'British', 'blur', true);
-
-const cat2 = new Cat('Murka', 3, 'British', 'blur', false);
-
-
-cat1.girfriend = cat2;
-
-cat1.girfriend.name;
-
-cat1.girfriend.name = 'Murka love'
-
-console.log(cat1.girfriend.name);
+const ob2 = new MyObj();
